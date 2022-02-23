eb050630ea417f143cc154b1feb26a2600697585
Ashish Rai <ash09rai@gmail.com>
2022-02-23 14:46:15 +0530

    Initial Commit

---
+++ FirstML/FirstMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstMLApp.swift
+//  FirstML
+//
+//  Created by Global on 23/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
